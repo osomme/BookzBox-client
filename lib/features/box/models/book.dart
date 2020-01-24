f364aad06d723bab0a1de6e1bdf71b53ca347a97
Oskar <osomme@gmail.com>
2020-01-24 11:14:52 +0100

    Auto stash before merge of "new-box" and "origin/new-box"

--- lib/features/box/models/book.dart
+++ lib/features/box/models/book.dart
@@ -14,3 +14,31 @@ class Book {
   Book(this.isbn, this.title, this.subtitle, this.thumbnailUrl, this.fullSizeImageUrl, this.author,
       this.pageCount, this.deweyDecimal, this.links, this.publishYear);
 }
+
+class Book2 {
+  final String isbn13;
+  final String isbn10;
+  final String title;
+  final String thumbnailUrl;
+  final String fullSizeImageUrl;
+  final List<String> authors;
+  final int pageCount;
+  final List<String> categories;
+  final int publishYear;
+  final String synopsis;
+  final String publisher;
+
+  Book2({
+    this.isbn13,
+    this.isbn10,
+    this.title,
+    this.thumbnailUrl,
+    this.fullSizeImageUrl,
+    this.authors,
+    this.pageCount,
+    this.categories,
+    this.publishYear,
+    this.synopsis,
+    this.publisher,
+  });
+}
