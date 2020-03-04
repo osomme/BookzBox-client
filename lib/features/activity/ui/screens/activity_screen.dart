841fd208b1fe19f53b50378f6401de38ab0d8a08
Oskar <osomme@gmail.com>
2020-03-04 12:08:45 +0100

    Added tabs

--- lib/features/activity/ui/screens/activity_screen.dart
+++ lib/features/activity/ui/screens/activity_screen.dart
@@ -1,6 +1,8 @@
 import 'package:bookzbox/features/activity/activity.dart';
+import 'package:bookzbox/features/profile/ui/widgets/custom_tab_bar.dart';
 import 'package:bookzbox/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class ActivityScreen extends StatefulWidget {
   @override
@@ -10,15 +12,47 @@ class ActivityScreen extends StatefulWidget {
 class _ActivityScreenState extends State<ActivityScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).activityScreenAppBarTitle),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-        child: ListView.builder(
-          itemCount: testActivityFeedItems.length,
-          itemBuilder: (ctx, i) => testActivityFeedItems[i].toListItem(context),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: CustomTabBar(
+          tabBar: TabBar(
+            indicatorColor: Theme.of(context).accentColor,
+            labelColor: Theme.of(context).accentColor,
+            unselectedLabelColor: Colors.white,
+            indicatorWeight: 3.5,
+            tabs: [
+              Tab(
+                text: 'Activity',
+                icon: Icon(Icons.notifications),
+              ),
+              Tab(
+                text: 'Messages',
+                icon: Icon(Icons.chat),
+              ),
+              Tab(
+                text: 'Likes',
+                icon: Icon(MaterialCommunityIcons.heart),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+              child: ListView.builder(
+                itemCount: testActivityFeedItems.length,
+                itemBuilder: (ctx, i) => testActivityFeedItems[i].toListItem(context),
+              ),
+            ),
+            Center(
+              child: Text('Messages tab'),
+            ),
+            Center(
+              child: Text('Likes tab'),
+            ),
+          ],
         ),
       ),
     );
