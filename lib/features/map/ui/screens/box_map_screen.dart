6d1e9227d39d96fc53a228cfba8e83ddf379c79c
Oskar <osomme@gmail.com>
2020-03-14 08:35:55 +0100

    Clusters added

--- lib/features/map/ui/screens/box_map_screen.dart
+++ lib/features/map/ui/screens/box_map_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:bookzbox/common/di/providers.dart';
 import 'package:bookzbox/features/authentication/authentication.dart';
@@ -7,6 +9,7 @@ import 'package:bookzbox/features/map/box_map.dart';
 import 'package:dartz/dartz.dart' as Dartz;
 import 'package:bookzbox/features/location/models/lat_lng.dart' as LatLngModel;
 import 'package:flutter/material.dart';
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
@@ -29,22 +32,35 @@ class _BoxMapScreenState extends State<BoxMapScreen> {
   // Default camera pos. (Halden, Norway)
   final startPos = const LatLng(59.1303617, 11.3543517);
 
+  List<ClusterItem<BoxMapItem>> items = List();
+
+  ClusterManager manager;
+
   Set<Marker> markers = Set();
 
   GoogleMapController mapController;
 
-  LatLng currentCamPos;
+  static const double _startingZoom = 12.0;
 
-  int iconSize = 84;
+  @override
+  void initState() {
+    manager = _initClusterManager();
+    super.initState();
+  }
 
-  int oldIconSize = 0;
+  ClusterManager<BoxMapItem> _initClusterManager() {
+    return ClusterManager<BoxMapItem>(items, _updateMarkers,
+        initialZoom: _startingZoom,
+        markerBuilder: _markerBuilder,
+        levels: const [5, 8.25, 11.5, 14.5, 16, 16.5, 20]);
+  }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     listeners.addAll([
-      boxesListener(),
-      errorListener(),
+      _boxesListener(),
+      _errorListener(),
     ]);
   }
 
@@ -59,17 +75,16 @@ class _BoxMapScreenState extends State<BoxMapScreen> {
     return Scaffold(
       body: SafeArea(
         child: GoogleMap(
+          minMaxZoomPreference: MinMaxZoomPreference(5.0, 15.0),
+          mapToolbarEnabled: false,
           markers: markers,
-          onMapCreated: onMapCreated,
+          onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: startPos,
-            zoom: 12.0,
+            zoom: _startingZoom,
           ),
-          onCameraIdle: onCameraIdle,
-          onCameraMove: (camPos) {
-            currentCamPos = camPos.target;
-            iconSize = calculateIconSize(camPos.zoom);
-          },
+          onCameraIdle: manager.updateMap,
+          onCameraMove: _onCameraMoved,
           myLocationButtonEnabled: true,
           myLocationEnabled: true,
         ),
@@ -77,13 +92,17 @@ class _BoxMapScreenState extends State<BoxMapScreen> {
     );
   }
 
-  void onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    listeners.add(userLocationListener());
-    reloadMarkers();
+  void _onCameraMoved(CameraPosition camPos) {
+    manager.onCameraMove(camPos);
   }
 
-  ReactionDisposer errorListener() {
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    manager.setMapController(controller);
+    listeners.add(_userLocationListener());
+  }
+
+  ReactionDisposer _errorListener() {
     return autorun((_) {
       if (widget.mapStore.error != null) {
         Scaffold.of(context).showSnackBar(
@@ -93,53 +112,24 @@ class _BoxMapScreenState extends State<BoxMapScreen> {
     });
   }
 
-  ReactionDisposer userLocationListener() =>
-      autorun((_) => onUserLocationObtained(widget.mapStore.userPosition));
+  ReactionDisposer _userLocationListener() =>
+      autorun((_) => _onUserLocationObtained(widget.mapStore.userPosition));
 
-  ReactionDisposer boxesListener() =>
-      autorun((_) => onBoxesLoaded(widget.mapStore.boxes));
+  ReactionDisposer _boxesListener() =>
+      autorun((_) => _onBoxesLoaded(widget.mapStore.boxes));
 
-  Future<void> onUserLocationObtained(
+  Future<void> _onUserLocationObtained(
       Dartz.Option<LatLngModel.LatLng> userLocation) async {
     final currentUserPos = userLocation
         .map((p) => LatLng(p.latitude, p.longitude))
         .getOrElse(() => startPos);
     await mapController.moveCamera(CameraUpdate.newLatLng(currentUserPos));
-    reloadMarkers();
   }
 
-  Future<void> onBoxesLoaded(List<BoxMapItem> boxes) async {
-    final markerIcon = await loadIcon();
-    setState(() {
-      markers = boxes.map((b) {
-        return Marker(
-          markerId: MarkerId(b.boxId),
-          position: LatLng(b.latitude, b.longitude),
-          onTap: () => buildDetailsWidget(b),
-          icon: markerIcon,
-        );
-      }).toSet();
-    });
-  }
-
-  void reloadMarkers() async {
-    if (iconSize == oldIconSize) {
-      // If icon size is the same as last time icons were updated, do not re-render them again, to save performance.
-      return;
-    }
-
-    final markerIcon = await loadIcon();
-    oldIconSize = iconSize;
-
-    print('Creating new markers with icon size: $iconSize');
-    setState(() {
-      markers = markers.map((m) => m.copyWith(iconParam: markerIcon)).toSet();
-    });
-  }
-
-  Future<BitmapDescriptor> loadIcon() async {
-    return await AssetLoader.getBytesFromAsset('assets/images/box-map-icon.png', iconSize)
-        .then((b) => BitmapDescriptor.fromBytes(b));
+  Future<void> _onBoxesLoaded(List<BoxMapItem> boxes) async {
+    final items =
+        boxes.map((b) => ClusterItem(LatLng(b.latitude, b.longitude), item: b)).toList();
+    manager.setItems(items);
   }
 
   void buildDetailsWidget(BoxMapItem box) {
@@ -168,7 +158,55 @@ class _BoxMapScreenState extends State<BoxMapScreen> {
     );
   }
 
-  int calculateIconSize(double zoomLevel) => max(zoomLevel.round() * 7, 60);
+  void _updateMarkers(Set<Marker> markers) => setState(() => this.markers = markers);
 
-  void onCameraIdle() => reloadMarkers();
+  // Taken from the example code for the google_maps_cluster_manager library:
+  // https://github.com/bpillon/google_maps_cluster_manager
+  Future<Marker> Function(Cluster<BoxMapItem>) get _markerBuilder => (cluster) async {
+        return Marker(
+          markerId: MarkerId(cluster.getId()),
+          position: cluster.location,
+          onTap: () {
+            print('Clicked on cluster with size: ${cluster.items.length}');
+            cluster.items.forEach((b) => print('${b.boxId}'));
+          },
+          icon: await _getMarkerBitmap(cluster.isMultiple ? 125 : 75,
+              text: cluster.isMultiple ? cluster.count.toString() : null),
+        );
+      };
+
+  // Taken from the example code for the google_maps_cluster_manager library:
+  // https://github.com/bpillon/google_maps_cluster_manager
+  Future<BitmapDescriptor> _getMarkerBitmap(int size, {String text}) async {
+    final PictureRecorder pictureRecorder = PictureRecorder();
+    final Canvas canvas = Canvas(pictureRecorder);
+    final Paint paint1 = Paint()..color = Theme.of(context).primaryColor;
+    final Paint paint2 = Paint()..color = Theme.of(context).accentColor;
+
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.0, paint1);
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.2, paint2);
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.8, paint1);
+
+    if (text != null) {
+      TextPainter painter = TextPainter(textDirection: TextDirection.ltr);
+      painter.text = TextSpan(
+        text: text,
+        style: TextStyle(
+          fontSize: size / 3,
+          color: Theme.of(context).primaryTextTheme.body1.color,
+          fontWeight: FontWeight.normal,
+        ),
+      );
+      painter.layout();
+      painter.paint(
+        canvas,
+        Offset(size / 2 - painter.width / 2, size / 2 - painter.height / 2),
+      );
+    }
+
+    final img = await pictureRecorder.endRecording().toImage(size, size);
+    final data = await img.toByteData(format: ImageByteFormat.png);
+
+    return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
+  }
 }
