b4a000dd344c25d02db3b0d9c0265773cf4ff995
htskole <38259894+HTSkole@users.noreply.github.com>
2020-05-21 17:01:34 +0200

    Improved test for negative numbers

--- test/utils/location_test.dart
+++ test/utils/location_test.dart
@@ -19,6 +19,18 @@ void main() {
       final double c = 59.267981;
       final double expectedC = 59.27;
       expect(Location.getCoordAt250mAccuracy(c), expectedC);
+
+      final double d = -59.263333;
+      final double expectedD = -59.265;
+      expect(Location.getCoordAt250mAccuracy(d), expectedD);
+
+      final double e = -59.262222;
+      final double expectedE = -59.26;
+      expect(Location.getCoordAt250mAccuracy(e), expectedE);
+
+      final double f = -59.267981;
+      final double expectedF = -59.27;
+      expect(Location.getCoordAt250mAccuracy(f), expectedF);
     });
   });
 }
